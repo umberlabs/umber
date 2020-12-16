58f7f7d1a3d050c74b7c1f189a62265c94dfbbad
js3r2 <bencamarata@wearehackerone.com>
2020-12-16 15:01:47 -0800

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Umber Updater
+
+echo "Umber Updater: Starting..."
+
+function err_exit() {
+    echo "Installation failed. Please try again."
+    exit 1
+}
+trap "err_exit" ERR
+
+export DEBIAN_FRONTEND=noninteractive
+
+read -r -p "Umber Updater: Update Umber? [Y/n] " hb
+case $hb in
+    [yY][eE][sS]|[yY])
+        echo "Umber Updater: Removing old Umber..."
+        sudo rm /usr/local/bin/um
+        echo "Umber Updater: Downloading new Umber..."
+        cd /opt
+        sudo mkdir uu
+        cd uu
+        sudo git clone https://github.com/js3r2/umber
+        cd umber
+        sudo mv um /usr/local/bin
+        echo "Umber Updater: Updated! Cleaning up..."
+        cd /opt
+        sudo rm -rf uu
+    ;;
+    [nN][oO]|[nN]|'')
+        echo "Canceled update."
+    ;;
+esac
