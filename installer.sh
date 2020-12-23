257fcb324c2bf0fce1d906b34239192f9e62f4c7
js3r2 <benmcamarata@gmail.com>
2020-12-23 15:29:27 -0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -7,30 +7,20 @@ function err_exit() {
 }
 trap "err_exit" ERR
 
-export DEBIAN_FRONTEND=noninteractive
-
-read -r -p "UM Installer: Install UM? [Y/n] " hb
-case $hb in
-    [yY][eE][sS]|[yY])
-        echo "UM Installer: Installing package umberlabs/main..."
-        cd /Users/$USER
-        git clone https://github.com/umberlabs/umber um
-        cd um
-        sudo mv um-prop /usr/local/bin
-        sudo chmod +x /usr/local/bin/um-prop
-        sudo chmod -R 777 /usr/local/bin/um-prop
-        sudo mv um /usr/local/bin
-        sudo chmod +x /usr/local/bin/um
-        sudo chmod -R 777 /usr/local/bin/um
-        mv update.sh um-update
-        sudo mv um-update /usr/local/bin
-        sudo chmod +x /usr/local/bin/um-update
-        sudo chmod -R 777 /usr/local/bin/um-update
-        cd /Users/$USER/
-        rm -rf um
-        echo "UM Installer: Summary: um, um-update & um-prop have been installed to /usr/local/bin."
-    ;;
-    [nN][oO]|[nN]|'')
-        echo "UM Installer: Skipping..."
-    ;;
-esac
+echo "UM Installer: Installing package umberlabs/main..."
+cd /Users/$USER
+git clone https://github.com/umberlabs/umber um
+cd um
+sudo mv um-prop /usr/local/bin
+sudo chmod +x /usr/local/bin/um-prop
+sudo chmod -R 777 /usr/local/bin/um-prop
+sudo mv um /usr/local/bin
+sudo chmod +x /usr/local/bin/um
+sudo chmod -R 777 /usr/local/bin/um
+mv update.sh um-update
+sudo mv um-update /usr/local/bin
+sudo chmod +x /usr/local/bin/um-update
+sudo chmod -R 777 /usr/local/bin/um-update
+cd /Users/$USER/
+rm -rf um
+echo "Finished!"
